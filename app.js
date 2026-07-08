1219903eddcbd58b387a3501d633db548d4bb2b3
rostisseriamilenium-alt <rostisseriamilenium@gmail.com>
2026-07-08 15:12:43 +0200

    Enhance stock management with kitchen items
    Added kitchen stock properties to the estado object and updated the actualizar function to display individual kitchen item stocks.

--- app.js
+++ app.js
@@ -2,7 +2,16 @@
 
 let estado = JSON.parse(localStorage.getItem("estado")) || {
     iniciado: false,
-    stock: 0
+    stock: 0,
+
+    cocina:{
+        caliu:0,
+        fritas:0,
+        bravas:0,
+        pan:0,
+        canelones:0,
+        pack1:0
+    }
 };
 
 const stockActual = document.getElementById("stockActual");
@@ -15,7 +24,12 @@ function guardar() {
 }
 
 function actualizar() {
-    stockActual.textContent = estado.stock;
+   document.getElementById("totalCaliu").textContent = estado.cocina.caliu;
+document.getElementById("totalFritas").textContent = estado.cocina.fritas;
+document.getElementById("totalBravas").textContent = estado.cocina.bravas;
+document.getElementById("totalPan").textContent = estado.cocina.pan;
+document.getElementById("totalCanelones").textContent = estado.cocina.canelones;
+document.getElementById("totalMediaCaliu").textContent = estado.cocina.pack1;
 
     stockInicial.disabled = estado.iniciado;
     btnIniciar.disabled = estado.iniciado;
